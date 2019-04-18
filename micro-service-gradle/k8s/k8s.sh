c9c54106ed21cac27653d4a3b840e351698c0cde
colddew <88028842@qq.com>
2019-04-18 22:25:02 +0800

    update k8s script

--- micro-service-gradle/k8s/k8s.sh
+++ micro-service-gradle/k8s/k8s.sh
@@ -9,9 +9,14 @@ watch -n 1 -d curl -HHost:<hostname> http://localhost:80/micro-service/api/v1/a
 watch -n 1 -d curl -HHost:<hostname> http://localhost:80/micro-service/api/v1/a/message
 watch -n 1 -d curl -HHost:<hostname> http://localhost:80/micro-service/api/v1/a/rest
 
-# sacle and upgrade micro-service
+# sacle micro-service
 kubectl scale -n micro-service --replicas=3 deployment/micro-service-a
 kubectl get pods -n micro-service -o=wide
 kubectl get service micro-service-a -n micro-service -o=wide
-kubectl describe deployment -n micro-service
+
+# upgrade micro-service
+kubectl describe deployment micro-service-a -n micro-service
 kubectl set image -n micro-service deployment/micro-service-a micro-service-a=cn.plantlink/micro-service-a:1.0.2-SNAPSHOT
+
+# rollout micro-service
+kubectl rollout undo deployment/micro-service-a -n micro-service
